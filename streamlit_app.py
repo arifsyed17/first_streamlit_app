1501e8658a6fbaaadd28347367344950554dce2f
arifsyed17 <151865863+arifsyed17@users.noreply.github.com>
2023-11-29 23:52:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,4 +36,5 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 #my_data_rows1 =my_data_rows.index
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+my_list = my_datarows.set_index('Fruit')
+streamlit.dataframe(my_list)
